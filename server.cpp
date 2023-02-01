0e240ce6f23b280cb6bf96da71cda5df97097156
sashanikina <47078185+sashanikina@users.noreply.github.com>
2023-02-01 18:00:50 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -14,13 +14,13 @@ int main()
 
 	ZeroMemory(&Overlapped, sizeof(Overlapped));
 
-	Overlapped.hEvent = CreateEvent(NULL, TRUE, 0, NULL);
+	Overlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
 	pipe_name = "\\\\.\\pipe\\mypipe";
-	pipe = CreateNamedPipeA(pipe_name.c_str(), PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED, PIPE_TYPE_BYTE | PIPE_WAIT, 1, 256, 256, 0, NULL);
-	BOOL isConnect = ConnectNamedPipe(pipe, &Overlapped);
-	if(!isConnect){
-		cout << "You can input data.\n\n";
+	pipe = CreateNamedPipeA(pipe_name.c_str(), PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED, PIPE_TYPE_BYTE | PIPE_WAIT, 1, 512, 512, 0, NULL);
+	ConnectNamedPipe(pipe, &Overlapped);
+	cout << "You can input data.\n\n";
+	while(true){
 		fgets(data, 512, stdin);
 		WriteFile(pipe, data, strlen(data) + 1, &written, &Overlapped);
 		WaitForSingleObject(Overlapped.hEvent, INFINITE);
@@ -28,9 +28,7 @@ int main()
 		delete[] data;
 		CloseHandle(Overlapped.hEvent);
 		DisconnectNamedPipe(pipe);
-	}
-	else {
-		cout << "Error creating pipe " << GetLastError() << endl;
+		break;
 	}
 	CloseHandle(pipe);
 	system("pause");
